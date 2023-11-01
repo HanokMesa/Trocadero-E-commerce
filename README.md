eb9a7307b81b168289b81ca4629fac0dfeba2335
unknown <hanokmesa3@gmail.com>
2023-11-01 23:31:57 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+<div align='center'><img src='https://user-images.githubusercontent.com/105128267/'/></div>
+
+<h2>About the project</h2>
+
+<p>This is a Traocadero e-commerce website built with React and CSS. This
+website features a modern and intuitive design, with easy-to-use navigation and a
+simple shopping experience that puts the focus on the products.</p>
+
+👉 Live Demo: <a href='https://trocadero-mhanok-ecommerce.netlify.app/'>Ecommerce Demo</a>
+
+<h3>Build with:</h3>
+
+» Vanila CSS <br>
+» React JS
+
+<h2>Screenshots of the Project 📸</h2>
+<br>
+<h3 align='center'>Home Page 🏡</h3>
+
+<div align='center'>
+<img src='https://user-images.githubusercontent.com/'/>
+</div>
+
+<br><br>
+<h3 align='center'>Categories Page 👇</h3>
+
+<div align='center'>
+<img src='https://user-images.githubusercontent.com/'/>
+
+<br>
+<br>
+<h3 align='center'>Product Page 🎁</h3>
+
+<div align='center'>
+<img src='https://user-images.githubusercontent.com/'/>
+
+<br>
+<br>
+<h3 align='center'>Shopping Cart 🛒</h3>
+
+<div align='center'>
+<img src='https://user-images.githubusercontent.com/'/>
+</div>
+
+
+
+
---
+++ src/components/AllData.js
@@ -0,0 +1,404 @@
+import stra1 from "../img/products/otherProducts/strandmon-wing-chair1.avif";
+import stra2 from "../img/products/otherProducts/strandmon-wing-chair2.avif";
+import stra3 from "../img/products/otherProducts/strandmon-wing-chair3.avif";
+import Img3 from "../img/products/3.png";
+import val1 from  "../img/products/otherProducts/val1.avif";
+import Img5 from "../img/products/5.jpg";
+import Img6 from "../img/products/6.jpg";
+import Img7 from "../img/products/7.jpg";
+import Img10 from "../img/products/10.jpg";
+import Img11 from "../img/products/11.jpg";
+import Img12 from "../img/products/12.png";
+import Img14 from "../img/products/14.png";
+import Img15 from "../img/products/15.jpg";
+import Img16 from "../img/products/16.jpg";
+import Img17 from "../img/products/17.png";
+import Img18 from "../img/products/18.png";
+import Img19 from "../img/products/19.jpg";
+import Img20 from "../img/products/20r.jpg";
+import LampBlue1 from "../img/products/otherProducts/lampblue1.webp";
+import LampBlue2 from "../img/products/otherProducts/lampblue2.webp";
+import val2 from  "../img/products/otherProducts/val2.avif";
+import val3 from  "../img/products/otherProducts/val3.avif";
+import ModernGold1 from "../img/products/otherProducts/moderngold1.webp";
+import ModernGold2 from "../img/products/otherProducts/moderngold2.webp";
+import CleaningOil1 from "../img/products/otherProducts/cleaningoil1.jpg";
+import CleaningOil2 from "../img/products/otherProducts/cleaningoil2.jpg";
+import BlackAndWhite1 from "../img/products/otherProducts/blackandwhite1.webp";
+import BlackAndWhite2 from "../img/products/otherProducts/blackandwhite2.webp";
+import BodyOil1 from "../img/products/otherProducts/bodyoil1.jpg";
+import BodyOil2 from "../img/products/otherProducts/bodyoil2.jpg";
+import Shelf1 from "../img/products/otherProducts/shelf1.avif";
+import Shelf2 from "../img/products/otherProducts/shelf2.avif";
+import shelf3 from "../img/products/otherProducts/shelf3.avif";
+import ChairBoom1 from "../img/products/otherProducts/chairboom1.webp";
+import ChairBoom2 from "../img/products/otherProducts/chairboom2.webp";
+import BlackLamp1 from "../img/products/otherProducts/blacklamp1.webp";
+import BlackLamp2 from "../img/products/otherProducts/blacklamp2.jpg";
+import GoldenLamp1 from "../img/products/otherProducts/goldenlamp1.webp";
+import GoldenLamp2 from "../img/products/otherProducts/goldenlamp2.webp";
+import WoodChair1 from "../img/products/otherProducts/woodchair1.webp";
+import WoodChair2 from "../img/products/otherProducts/woodchair2.webp";
+import Copenhagen1 from "../img/products/otherProducts/copenhagen1.webp";
+import Copenhagen2 from "../img/products/otherProducts/copenhagen2.webp";
+import Cam1 from "../img/products/otherProducts/cam1.webp";
+import Cam2 from "../img/products/otherProducts/cam2.jpg";
+import TableLamp1 from "../img/products/otherProducts/tablelamp1.webp";
+import TableLamp2 from "../img/products/otherProducts/tablelamp2.webp";
+import MatLamp1 from "../img/products/otherProducts/mat1.webp";
+import MatLamp2 from "../img/products/otherProducts/mat2.webp";
+import Comfy1 from "../img/products/otherProducts/comfy1.webp";
+import Comfy2 from "../img/products/otherProducts/comfy2.webp";
+import Atuf from "../img/products/otherProducts/ASUS_TUF_Gaming_F15.webp";
+import tuf2 from "../img/products/otherProducts/ASUS_TUF_Gaming_F15.webp";
+import tuf1 from "../img/products/otherProducts/tuf1.webp";
+import lenv from "../img/products/otherProducts/Lenovo _deaPad.webp";
+import len1 from "../img/products/otherProducts/len2.webp";
+import len2 from "../img/products/otherProducts/len1.webp";
+import forg from "../img/products/foergylla1.avif";
+import forg2 from "../img/products/foergylla2.avif";  
+import forg3 from "../img/products/foergylla3.avif"; 
+import lagan1 from "../img/products/otherProducts/lagan1.avif"
+import lagan2 from  "../img/products/otherProducts/lagan2.avif"
+import lagan3 from  "../img/products/otherProducts/lagan3.avif"
+import draw1 from "../img/products/otherProducts/drawer1.avif"
+import draw2 from "../img/products/otherProducts/drawer2.avif"
+import draw3 from "../img/products/otherProducts/drawer3.avif"
+import tum1 from "../img/products/otherProducts/tumholmen1.avif"
+import tum2 from "../img/products/otherProducts/tum2.avif"
+import tum3 from "../img/products/otherProducts/tun3.avif"
+import pm1 from "../img/products/otherProducts/pm1.avif"
+import pm2 from "../img/products/otherProducts/pm2.avif"
+import pm3 from "../img/products/otherProducts/pm3.avif"
+import t1 from "../img/products/otherProducts/t2.jpg"
+import t3 from "../img/products/otherProducts/t3.jpg"
+import t2 from "../img/products/otherProducts/t4.jpg"
+
+
+export const items = [
+  {
+    id: 1,
+    category: "chair",
+    img: stra1,
+    description: "STRANDMON Armchair and footstool",
+    price: 23980,
+    otherImgs: [stra2, stra3],
+    specs:
+   "STRANDMON has a timeless look with modern comfort for many cosy moments. The wing chair offers a soft embracing feel and the footstool is nice to rest your feet on – or use as extra seating for guests. Round armrests, soft lines and an inviting upholstery give this wing chair a soft embracing feel.",
+    texture: "polyester fabric",
+    weight: "35kg",
+    size: "41cm x 32cm",
+  },
+  {
+    id: 2,
+    category: "kitchen",
+    img: t3,
+    description: "Philips Pop-Up Toaster",
+    price: 4500,
+    otherImgs: [t1, t2],
+    specs:
+      " The rounded square design of the toaster allows it to fit neatly against a wall, or inside a corner. Excellent at defrosting, and with the perfect breadtoheater distance to give crunchy toast with a soft, spongey middle.",
+    texture: "Plastic",
+    weight: "1.14kg",
+    size: "20xm x 10cm",
+  },
+  {
+    id: 3,
+    category: "lamp",
+    img: Img3,
+    description: "Lamp Light Blue",
+    price: 7900,
+    otherImgs: [LampBlue1, LampBlue2],
+    specs:
+      "The Verner Panton Flowerpot , designed in 1968, was originally intended for the restaurant industry and Verner Panton exhibitions.",
+    texture: "Plastic",
+    weight: "2kg",
+    size: "15cm x 15cm",
+  },
+  {
+    id: 4,
+    category: "furniture",
+    img: val3,
+    description: "VALEVÅG sprung mattress",
+    price: 14400,
+    otherImgs: [val2, val1],
+    specs:
+      "A firm 24 cm high mattress with individual pocket springs. Even distribution of body weight by comfort zones offers softer support for hips and shoulders. A thick layer of foam adds to the comfort. The 5 comfort zones in the mattress give very precise support and relieve pressure on your shoulders and hips.",
+    texture: "Non-woven polypropylene",
+    weight: "25kg",
+    size: "160x200cm",
+  },
+  {
+    id: 5,
+    category: "lamp",
+    img: Img5,
+    description: "Golden Modern Light",
+    price: 4890,
+    otherImgs: [ModernGold1, ModernGold2],
+    specs:
+      "Tom Dixon and FRONT have created an incredibly beautiful pendant, in a completely unique design. The lamp is made using a special metallization technology that makes the look completely unique. The first of its kind. the lamp is transparent when its on and you can see the exciting play of colors.",
+    texture: "Plastic",
+    weight: "3kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 6,
+    category: "skin-care",
+    img: Img6,
+    description: "Body Oil 200ml",
+    price: 6700,
+    otherImgs: [CleaningOil1, CleaningOil2],
+    specs:
+      "Made from 100% plant-based oils with olive oil as the main ingredient, this cleansing oil is gentle on the skin. Free from fragrance, colours and, mineral oils, paraben free, alcohol free, allergy tested (we cannot guarantee that it will not cause allergies in any users) * How to use: Take a small amount in the palm of your hand, and massage into the face to remove make-up and dirt. Rinse well with cold or warm water",
+    texture: "Liquid",
+    weight: "200ml",
+    size: "10cm x 5cm",
+  },
+  {
+    id: 7,
+    category: "lamp",
+    img: Img7,
+    description: "Black and White Lamp",
+    price: 2500,
+    otherImgs: [BlackAndWhite1, BlackAndWhite2],
+    specs:
+      "Concept: Dot is inspired by perforated metal and the patterns that light creates when it shines through the small holes. The contrast between the coarse perforated metal and the smooth opal glass makes the Dot a simple and meaningful pendant. With its elegant expression, Shine, whether the light is on or off.",
+    texture: "Plastic",
+    weight: "6kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 8,
+    category: "furniture",
+    img: Shelf2,
+    description: "EKENABBEN Open shelving unit",
+    price: 5990,
+    otherImgs: [Shelf1, shelf3],
+    specs:
+      "Store all your things, customise as needed, sand or paint to freshen up – and when you are done with EKENABBEN shelving unit, you can let someone reuse the entire shelf or recycle the material. The shelving section is made of untreated wood – so you can freshen it up with sandpaper or paint it to make it more personal.",
+    texture: "Wood",
+    weight: "20kg",
+    size: "70x34x154cm",
+  },
+  {
+    id: 9,
+    category: "furniture",
+    img: draw1,
+    description: "ALEX Drawer unit",
+    price: 9490,
+    otherImgs: [draw2, draw3],
+    specs:
+    "A clean look that’s easy to like and mix with other styles, either supporting a desk or standing alone. The back is finished so you can place it in the middle of the room – all sides are just as beautiful. This unit can be placed anywhere in the room because it is finished on the back.",
+    texture: "Wood",
+    weight: "12kg",
+    size: "67x66 cm",
+  },
+  {
+    id: 10,
+    category: "chair",
+    img: Img10,
+    description: "Traditional Armchair",
+    price: 8900,
+    otherImgs: [ChairBoom1, ChairBoom2],
+    specs:
+      "Concept: &Tradition's Boomerang lounge chair is a classic Danish design with a modern and minimalist expression. The chair was designed by Hvidt & Mølgaard in 1956, and with its stylish cushions and hand-polished wooden frame, it is an ideal Nordic design that gives a personal mark to the home.",
+    texture: "Wood",
+    weight: "8kg",
+    size: "60cm x 40cm",
+  },
+  {
+    id: 11,
+    category: "lamp",
+    img: Img11,
+    description: "The Sun Pendant Black",
+    price: 4600,
+    otherImgs: [BlackLamp1, BlackLamp2],
+    specs:
+      "The Here Comes The Sun pendant designed by Bertrand Balas is a quality lamp with a unique and magical look. The Here Comes The Sun series has a fantastic expression and especially swhen turned on it creates a very special atmosphere in the room it hangs in. The lamp fits perfectly above both dining room table, kitchen counter, and as living room lighting . ",
+    texture: "Plastic",
+    weight: "4kg",
+    size: "20cm x 20cm",
+  },
+  {
+    id: 12,
+    category: "lamp",
+    img: Img12,
+    description: "Simple Golden Lamp",
+    price: 4200,
+    otherImgs: [GoldenLamp1, GoldenLamp2],
+    specs:
+      "Verner Panton Flowerpot, designed in 1968, was originally meant for the hospitality industry and Verner Panton displays and showrooms. The lamp then became popular in private homes, and due to its stylish design and many colour choices, it has made a major comeback in recent years.",
+    texture: "Plastic",
+    weight: "4kg",
+    size: "15cm x 15cm",
+  },
+  {
+    id: 13,
+    category: "chair",
+    img: pm1,
+    description: "POÄNG Rocking-chair",
+    price: 10900,
+    otherImgs: [pm2,pm3],
+    specs:
+      "Timeless design and endless comfort have made POÄNG a beloved armchair for over 40 years. As a rocking chair, the bent shape is retained and the comfortable resilience reaches new heights. Enjoy rocking!",
+    texture: "wood",
+    weight: "10kg",
+    size: "56cm x 45cm",
+  },
+  {
+    id: 14,
+    category: "chair",
+    img: Img14,
+    description: "Oak Spanish Chair",
+    price: 5200,
+    otherImgs: [WoodChair1, WoodChair2],
+    specs:
+      "When talking about Børge Mogensen, The Spanish Chair is probably one of the first designs you would think of, which makes good sense as it is also one of his most recognized products around the world.",
+    texture: "Wood",
+    weight: "15kg",
+    size: "60cm x 30cm",
+  },
+  {
+    id: 15,
+    category: "chair",
+    img: Img15,
+    description: "Copenhagen Armchair",
+    price: 3300,
+    otherImgs: [Copenhagen1, Copenhagen2],
+    specs:
+      "Concept: The Hyg series by Danish Normann Copenhagen is a series of chairs based on the Danish word Hygge. The designer Simon Legald has tried to embody the meaning of the word around security, warmth and convenience in a furniture series. This is expression in the fine organic shapes and hearty curves that provide a high degree of sitting comfort and well-being in the chairs. ",
+    texture: "Wood",
+    weight: "14kg",
+    size: "65cm x 35cm",
+  },
+  {
+    id: 16,
+    category: "electronic",
+    img: Img16,
+    description: "Anti Dark Light",
+    price: 6200,
+    otherImgs: [Cam1, Cam2],
+    specs:
+      "Easy Mini W75 from Antidark lives fully up to its name. Simple design, comfortable light, easy to adjust. The Easy Wall Light has made it easy for the user to handle, well, everything! The Easy lamp can turn 360 degrees and be tipped. An ideal lamp that can be placed in connection with other lamps to achieve light in all corners of the room. Furthermore, the lamp can work as a wall light or ceiling",
+    texture: "Metal",
+    weight: "2kg",
+    size: "15cm x 10cm",
+  },
+  {
+    id: 17,
+    category: "lamp",
+    img: Img17,
+    description: "Table Lamp",
+    price: 7500,
+    otherImgs: [TableLamp1, TableLamp2],
+    specs:
+      "The lamp has also become popular in private homes and has especially made a comeback in recent years. This is not least due to the lamp's stylish design and the many color options that make it fit everywhere.",
+    texture: "Metal",
+    weight: "5kg",
+    size: "25cm x 15cm",
+  },
+  {
+    id: 18,
+    category: "lamp",
+    img: Img18,
+    description: "Mat Black Lamp",
+    price: 4075,
+    otherImgs: [MatLamp1, MatLamp2],
+    specs:
+      "Unique offer We give you a special discount when you put this product in the basket. *Only applies to specific products.",
+    texture: "Metal",
+    weight: "6kg",
+    size: "20cm x 15cm",
+  },
+  {
+    id: 19,
+    category: "chair",
+    img: Img19,
+    description: "Comfy Chair",
+    price: 10700,
+    otherImgs: [Comfy1, Comfy2],
+    specs:
+      "Sum is a series designed by the Danish designer Simon Legald, who is the epitome of modern elegance. Elegant and resolute as the tip of a wing, the armrests spread out invitingly, as if they want to embrace you in the armchair's soft, homely space.",
+    texture: "Comfy Material",
+    weight: "25kg",
+    size: "60cm x 30cm",
+  },
+  {
+    id: 20,
+    category: "skin-care",
+    img: Img20,
+    description: "Body Oil 50ml",
+    price: 4500,
+    otherImgs: [BodyOil1, BodyOil2],
+    specs:
+      "Made from 100% plant-based oils with olive oil as the main ingredient, this cleansing oil is gentle on the skin. Free from fragrance, colours and, mineral oils, paraben free, alcohol free, allergy tested (we cannot guarantee that it will not cause allergies in any users) * How to use: Take a small amount in the palm of your hand, and massage into the face to remove make-up and dirt. Rinse well with cold or warm water.",
+    texture: "Liquid",
+    weight: "50ml",
+    size: "10cm x 5cm",
+  },
+  {
+    id: 21,
+    category: "electronic",
+    img: Atuf,
+    description: "ASUS TUF Gaming F15",
+    price: 102990,
+    otherImgs: [tuf1, tuf2],
+    specs:
+      "15.6 Inch Full HD, Refresh Rate 144Hz, Viewing Angle 170, Panel Tech Value IPS-level, Brightness 250nits, Contrast 1000:1, NTSC 45%, SRGB 62.50%, Adobe 47.34%, Anti-glare display , Light Laptop without Optical Disk Drive with Preloaded with MS Office.",
+    texture: "Plastic",
+    weight: "2.20kg",
+    size: "15.6 Inch",
+  },
+  {
+    id: 22,
+    category: "electronic",
+    img: lenv,
+    description: "Lenovo IdeaPad Gaming 3",
+    price: 1010000,
+    otherImgs: [len1, len2],
+    specs:
+    "16 Inch WUXGA IPS 350nits Anti-glare, 165Hz,Intel Core i7 12th Gen,100% sRGB, DC dimmer.Light Laptop without Optical Disk Drive. Preloaded with MS Office",
+    texture: "Plastic",
+    weight: "2.6kg",
+    size: "16 Inch",
+  },
+  {
+    id: 23,
+    category: "electronic",
+    img: forg,
+    description: "FÖRGYLLA Gas hob, black",
+    price: 21990,
+    otherImgs: [forg2, forg3],
+    specs:
+    " Here is a hob that lets you cook with more variety. It is ideal for stir frying as the wok burner delivers a rapid high-powered heat. The sturdy cast iron supports keep your wok firmly in place",
+    texture: "Glass",
+    weight: "14.60kg",
+    size: "65 cm ",
+  },
+  {
+    id: 24,
+    category: "kitchen",
+    img: lagan1,
+    description: "LAGAN dishwasher",
+    price: 42990,
+    otherImgs: [lagan2, lagan3],
+    specs:
+      " A reliable dishwasher that has all the basic functions you need to get the job done. Equipped with electric salt indicator. The softener makes limy water soft for best dish-washing result and prevents harmful scale forming in the dishwasher.",
+    texture: "stainless steel ",
+    weight: "35kg",
+    size: "60cm",
+  },
+  {
+    id: 25,
+    category: "chair",
+    img: tum3,
+    description: "TUMHOLMEN Rocking-Chair",
+    price: 5490,
+    otherImgs: [tum2,tum1],
+    specs:
+   "A rocker for relaxing. The simple design with a colourful, waterproof and easy-care textile seat, attracts the eye, adding a cosy vibe to your outdoor space – but is just and comfy and cool indoors too.The cover is easy to keep clean and fresh, as you can take it off and machine-wash it.",
+    texture: "polyester fabric",
+    weight: "35kg",
+    size: "55cm x 84cm",
+  },
+];
